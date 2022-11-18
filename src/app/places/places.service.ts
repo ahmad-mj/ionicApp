0ca7bf7734d96bf25752faa28a5a79523034dcce
Ahmad Al Mujawer <ahmad@prototype.berlin>
2022-11-18 11:32:56 +0100

    update hard codede data

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -8,22 +8,22 @@ export class PlacesService {
   private _items: Place[] = [
     new Place(
       'p1',
-      'Berlin Cosy apartment',
-      'nice place near a lake',
-      'https://berlin.homecompany.de/images/immodb2/4/23422/wohnen.jpg',
+      'Berlin, Kreuzberg Special Experience',
+      'Calm place, where you can rest, hear chill music and smoke with nice folks',
+      'https://c8.alamy.com/compde/b021f8/coffeeshop-amsterdam-coffee-shop-gemeinsame-haschisch-gras-canabis-marihuana-schmiere-marihuana-drogen-cannabis-b021f8.jpg',
       91
     ),
     new Place(
       'p2',
-      'Paris fancy apartment',
+      'Berlin, Mitte fancy Hash Shop',
       'city center',
       'https://cdn.thespaces.com/wp-content/uploads/2017/02/paris-apartments-for-rent-Rue-de-Varenne-III-3-1.jpg',
       199.99
     ),
     new Place(
       'p3',
-      'NewYork Great spot',
-      'city center',
+      'Alexander-Platz Great spot',
+      'Stain the day and get high in the highest cannabis spot in Berlin ',
       'https://media.architecturaldigest.com/photos/60897ad53d446d6023247999/master/pass/viper2.jpg',
       299.99
     ),
