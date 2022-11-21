8546c10d088f49687fcffeedab5043d39e75810e
Ahmad Al Mujawer <ahmad@prototype.berlin>
2022-11-21 11:44:06 +0100

    Render hardcoded bookings data and add sliding functionality

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
+import { BookingService } from './booking.service';
+import { Booking } from './bookings.model';
 
 @Component({
   selector: 'app-bookings',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
-
-  constructor() { }
+  loadedItems: Booking[];
+  constructor(private bookingService: BookingService) {}
 
   ngOnInit() {
+    this.loadedItems = this.bookingService.bookings;
   }
 
+  public cancelBooking(id: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    //Todo: cancel booking with id
+  }
 }
