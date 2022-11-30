a5c0f773a033b2a9e4156d14eee7cdd649720acb
Ahmad Al Mujawer <ahmad@prototype.berlin>
2022-11-30 15:35:39 +0100

    Add actionSheetController - booking date etc...

--- src/app/places/find/place-detail/place-detail.page.ts
+++ src/app/places/find/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
+import { ActionSheetController, ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places.service';
@@ -18,7 +18,9 @@ export class PlaceDetailPage implements OnInit {
     private route: ActivatedRoute,
     private navController: NavController,
     private placesService: PlacesService,
-    private modalController: ModalController,
+    public modalCtrl: ModalController,
+    private actionSheetController: ActionSheetController,
+
   ) {}
 
   ngOnInit() {
@@ -30,13 +32,58 @@ export class PlaceDetailPage implements OnInit {
       this.item = this.placesService.loadItems(paramMap.get('id'));
     });
   }
-  onClick() {
+  public async onClick() {
     // this.router.navigate(['main/tabs/explore']);
-    this.modalController.create({
-    component: CreateBookingComponent,
-    componentProps: {booked: this.item}
-    }).then(modalElement => {modalElement.present();
-    modalElement.onDidDismiss();
-    });
+
+
+ await this.actionSheetController.create({
+        header: 'Albums',
+        buttons: [{
+          text: 'Date',
+          icon: 'calendar',
+          role: 'confirm',
+          handler: () => {
+            console.log('Date clicked');
+            this.openBookingModal('select');
+          }
+        }, {
+          text: 'Share',
+          icon: 'share',
+          handler: () => {
+            console.log('Share clicked');
+            this.openBookingModal('random');
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+
+          }
+        }]
+      }).then(async ele => {
+        await ele.present();
+      });
+
   }
+
+  public async openBookingModal(mode: 'select' | 'random') {
+    console.log('Mode', mode );
+    const bookingModal = await this.modalCtrl.create({
+      component: CreateBookingComponent,
+      componentProps: {booked: this.item}
+      });
+
+      await bookingModal.present();
+      const { data } = await bookingModal.onDidDismiss();
+      console.log(data);
+      console.log('role: ',data.role);
+      if (data.role === 'confirm') {
+      console.log('BOOKED!:  ',data.role);
+
+      }
+
+  }
+
 }
