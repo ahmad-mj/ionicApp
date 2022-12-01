9b0f0d0c1888eb43c05340a86c0d45d2451990bb
Ahmad Al Mujawer <ahmad@prototype.berlin>
2022-12-01 16:02:47 +0100

    Add form and logic to add a new-offer <ion-date-time> still not working properly

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-offer',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public form: FormGroup;
+  datePicker = false;
+  public dateValue = '2022-11-22T00:00:00+01:00';
+  public dateMin: Date = null;
+  public dateMax: Date = null;
+  constructor() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(200)],
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)],
+      }),
+      dateFrom: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+    });
   }
 
+  ngOnInit() {}
+  public createOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+    console.log('Add logic to add a new-offer');
+  }
+  public showDatePicker() {
+    this.datePicker = !this.datePicker;
+    console.log('datepicker', this.datePicker);
+  }
+  public dateChanged(value) {
+    this.dateValue = value;
+    this.datePicker = false;
+    console.log(value);
+  }
 }
